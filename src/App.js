7bcff01e9dd24ddcf0825e4155e2bd9d7dcc3983
joshdidcote <joshua.didcote@outlook.com>
2022-12-19 18:02:00 +0000

    Refactor app content into todo component

--- src/App.js
+++ src/App.js
@@ -1,83 +1,10 @@
 import { useState } from "react";
-import CreateTodo from "./components/CreateTodo";
-import Todo from "./components/Todo";
+import CreateTodo from "./components/todos/CreateTodo";
+import Todo from "./components/todos/Todo";
 import "./index.css";
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [showCreateTodo, setShowCreateTodo] = useState(false);
-
-  const updateTodoStatus = (todoId, completionStatus) => {
-    const idToModify = todos.filter((todo) => todo.id == todoId);
-
-    const newTodos = todos.map((todo) => {
-      if (todo.id === todoId) {
-        return { ...todo, isComplete: completionStatus };
-      } else {
-        return todo;
-      }
-    });
-    setTodos(newTodos);
-  };
-
-  const handleDisplayCreateTodo = () => {
-    return showCreateTodo ? "block" : "hidden";
-  };
-
-  const isNewTodoTitleUnique = (todo) => {
-    // Checks if the title for the new todo already exists
-    let isUnique = true;
-    todos.map((item) => {
-      if (todo.title === item.title) {
-        isUnique = false;
-      }
-    });
-    return isUnique;
-  };
-
-  const handleAddTodo = (todo) => {
-    if (!isNewTodoTitleUnique(todo)) {
-      alert("Todo titles need to be unique");
-      return;
-    }
-    todo.id = todos.length;
-    setTodos(todos.concat(todo));
-    setShowCreateTodo(false);
-  };
-
-  return (
-    <div className="font-poppins container md m-auto">
-      <header className="container mx-auto p-6">
-        <h1 className="flex items-center justify-center text-4xl font-medium">
-          To-do App
-        </h1>
-        <div className="flex items-center justify-center">
-          <button
-            className="hover:bg-blue-200 rounded-lg bg-blue-300 p-2 pt-1 pb-1 mt-4 ml-2 mr-2"
-            onClick={() => setShowCreateTodo(true)}
-          >
-            Add item
-          </button>
-        </div>
-      </header>
-      <CreateTodo
-        handleSubmit={handleAddTodo}
-        isTodoVisible={handleDisplayCreateTodo}
-        setShowCreateTodo={setShowCreateTodo}
-      />
-      <div className="flex flex-col w-2/6 m-auto">
-        {todos.map((todo) => {
-          return (
-            <Todo
-              todo={todo}
-              key={todo.id}
-              updateTodoStatus={updateTodoStatus}
-            />
-          );
-        })}
-      </div>
-    </div>
-  );
+  return <Todo></Todo>;
 }
 
 export default App;
---
+++ src/components/todos/CreateTodo.js
@@ -0,0 +1,109 @@
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+export default function CreateTodo(props) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [dueDate, setDueDate] = useState(new Date());
+  const [priority, setPriority] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!title) {
+      alert("Todo title cannot be empty");
+      return;
+    }
+
+    props.handleSubmit({
+      title,
+      description,
+      dueDate,
+      priority,
+      isComplete: false,
+    });
+  };
+
+  const handleClose = (e) => {
+    e.preventDefault();
+    props.setShowCreateTodo(false);
+  };
+
+  const formLabel = "text-slate-500 md:w-1/3 flex-none";
+  const formSection =
+    "flex space-x-4 justify-start items-center mb-3 mt-3 ml-5 mr-4";
+  const inputStyle = "bg-slate-200 rounded px-2";
+
+  return (
+    <div
+      className={
+        "container mx-auto border-4 max-w-sm flex flex-row rounded-md border-slate-400 text-center absolute left-0 right-0 bg-white " +
+        props.isTodoVisible()
+      }
+    >
+      <form>
+        <div className={formSection}>
+          <label className={formLabel}>Title</label>
+          <input
+            className={inputStyle}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+            value={title}
+            required
+          ></input>
+        </div>
+
+        <div className={formSection}>
+          <label className={formLabel}>Description</label>
+          <input
+            className={inputStyle}
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+            value={description}
+          ></input>
+        </div>
+
+        <div className={formSection}>
+          <label className={formLabel}>Due date</label>
+          <DatePicker
+            className={inputStyle}
+            onChange={(e) => {
+              setDueDate(e);
+            }}
+            selected={dueDate}
+          ></DatePicker>
+        </div>
+
+        <div className={formSection}>
+          <label className={formLabel}>Priority</label>
+          <input
+            className={inputStyle}
+            onChange={(e) => {
+              setPriority(e.target.value);
+            }}
+            value={priority}
+            type="number"
+          ></input>
+        </div>
+
+        <div className="flex justify-center m-auto ml-28 w-max mt-0">
+          <button
+            onClick={handleClose}
+            className="hover:bg-red-200 rounded-lg bg-red-300 p-2 pt-1 pb-1 mt-0 mb-2 mx-2"
+          >
+            Close
+          </button>
+          <button
+            onClick={handleSubmit}
+            className="hover:bg-green-200 rounded-lg bg-emerald-300 p-2 pt-1 pb-1 mt-0 mb-2 mx-2"
+          >
+            Save
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
